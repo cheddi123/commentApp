3909d63804bacf7c7abf65bfb02ac01af42e7a73
CHEDDI-PC <cheddi.charles@gmail.com>
2023-06-15 14:26:07 -0500

    creating user routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,10 +1,27 @@
 const express = require('express');
 const router = express.Router()
 
-router.get("/user",(req,res)=>{
-    res.json("I am a user ")
+// get login page
+router.get("/login",(req,res)=>{
+    res.json("I am logging to enter into the app ")
 }
 );
 
+// Post login 
+router.post("/login",(req,res)=>{
+    console.log(req.body)
+    res.json(req.body)
+})
 
+// Register a new user 
+router.post("/register",(req,res)=>{
+    console.log(req.body)
+    res.json(req.body)
+})
+
+// Logout 
+router.get("/logout",(req,res)=>{
+    console.log("I am logging out")
+    res.send("I am logging out")
+})
 module.exports=router
